275ac6611bc62aa04643f4034190533c0b6a4591
Johanlex <127138253+Johanlex20@users.noreply.github.com>
2024-02-21 14:02:01 -0500

    Add interface user.ts, para implementarla en la función loginServices para que reconozca una plantilla de datos a Observar

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { LoginRequest } from './loginRequest';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,9 @@ export class LoginService {
 
   constructor(private http:HttpClient) {}
 
-  login(credentials:LoginRequest):Observable<any>{
+  login(credentials:LoginRequest):Observable<User>{
     //console.log(credentials);
-    return this.http.get('././assets/data.json')
+    return this.http.get<User>('././assets/data.json')
   }
 
 }
